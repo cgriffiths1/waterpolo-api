8c28947a01c6246dbb5fd36b9344d2a8f893fbc9
cgriffiths1 <cgriffiths1@companyhouse.gov.uk>
2022-02-28 15:28:41 +0000

    First commit

---
+++ src/main/java/uk/gov/companieshouse/waterpoloapi/WaterpoloApiApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.waterpoloapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterpoloApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterpoloApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/gov/companieshouse/waterpoloapi/WaterpoloApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.waterpoloapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterpoloApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
